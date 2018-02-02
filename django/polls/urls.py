73010aa8abba6311fd850b616da798344bad828f
chrisaor <pchpch0070@gmail.com>
2018-02-02 14:33:17 +0900

    detail, results, vote뷰 추가 및 url연결

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    # re_path를 사용하여 정규표현식으로 구현해보기
+    # re_path('^$', views.index, name='index'),
+    # re_path('^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    # re_path('^(?P<question_id>\d+)/result/$', views.results, name='results'),
+    # re_path('^(?P<question_id>\d+)/vote/$', views.vote, name='vote')
 ]
