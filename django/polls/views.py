c73e632e123fec47a0604dfa797f8771af2787a5
chrisaor <pchpch0070@gmail.com>
2018-02-02 15:28:50 +0900

    detail view에 Choice목록을 출력

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 # Create your views here.
@@ -13,15 +13,24 @@ def index(request):
     # 쉼표단위로 구분된 Question목록의 각 항목의 Question_text로 만들어진 문자열
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     return render(request, 'polls/index.html', context)
     # 장고 공식문서는 다음과 같다.
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #    'latest_question_list': latest_question_list,
+    # }
     # return HttpResponse(template.render(context, request))
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
